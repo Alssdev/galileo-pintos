a637feb2d157214b4cf469e108d2250a61b7fb77
Alssdev <alssdev3@gmail.com>
2024-03-18 10:32:35 -0600

    feat(syscalls): open syscall first implementation

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,19 +1,22 @@
 #include "devices/shutdown.h"
 #include "userprog/syscall.h"
+#include "filesys/file.h"
+#include "list.h"
 #include "threads/interrupt.h"
 #include "userprog/process.h"
+#include "filesys/filesys.h"
 #include "threads/thread.h"
 #include "threads/synch.h"
+#include "threads/malloc.h"
 #include "filesys/off_t.h"
 #include <syscall-nr.h>
 #include <stdio.h>
 #include <stdint.h>
-#include "filesys/filesys.h"
 
 static void syscall_handler (struct intr_frame *);
 static uint32_t stack_arg (void *esp, uint8_t offset);
 
-/* handlers */
+/* handlers. */
 static void write_handler (struct intr_frame *);
 static void exit_handler (struct intr_frame *);
 static void exec_handler (struct intr_frame *);
@@ -21,11 +24,26 @@ static void halt_handler (void);
 static void wait_handler (struct intr_frame *);
 static void remove_handler(struct intr_frame *f);
 static void create_handler(struct intr_frame *f);
+static void open_handler(struct intr_frame *f);
 
+/* file system */
+struct lock filesys_lock;               /* synchronization for the file system. */
+struct list fds;                        /* file descriptor list. */
+struct fd_elem {
+  struct list_elem elem;
+  struct file *file;
+  int fd;                               /* file descriptor. */
+};
+int next_fd;                            /* file descriptor counter. */ 
 
 void
 syscall_init (void)
 {
+  /* structs MUST be initilizated before enabling syscalls. */
+  lock_init(&filesys_lock);
+  list_init(&fds);
+  next_fd = 2;                          /* 0 and 1 are reserved form stdo and stdi. */
+
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
@@ -62,6 +80,10 @@ syscall_handler (struct intr_frame *f)
       create_handler(f);
       break;
 
+    case SYS_OPEN:
+      open_handler(f);
+      break;
+
     default:
       printf("system call %x !\n", sys_code);
       thread_exit ();                           /* thread_exit calls process_exit */
@@ -85,6 +107,8 @@ stack_arg (void *esp, uint8_t offset)
 static void
 write_handler (struct intr_frame *f)
 {
+  lock_acquire(&filesys_lock);
+
   uint32_t fd = stack_arg(f->esp, 1);
   char *buffer = (char*)stack_arg(f->esp, 2);       /* mem[buffer] contains the string. */
   unsigned size = (unsigned)stack_arg(f->esp, 3);   /* bytes to be printed. */
@@ -95,8 +119,11 @@ write_handler (struct intr_frame *f)
   } else {
     /* TODO: complete write syscall */
     printf("write syscall!\n");
+    lock_release(&filesys_lock);
     thread_exit ();
   }
+
+  lock_release(&filesys_lock);
 }
 
 static void exit_handler (struct intr_frame *f) {
@@ -121,14 +148,46 @@ static void wait_handler (struct intr_frame *f) {
   f->eax = exit_status;
 }
 
-static void remove_handler(struct intr_frame *f){
+static void remove_handler(struct intr_frame *f) {
+  lock_acquire(&filesys_lock);
+
   char *name = (char*)stack_arg(f->esp, 1);
   f->eax = filesys_remove(name);
+ 
+  lock_release(&filesys_lock);
 }
 
-static void create_handler(struct intr_frame *f){
+static void create_handler(struct intr_frame *f) {
+  lock_acquire(&filesys_lock);
+
   char* name = (char*)stack_arg(f->esp, 1);
   off_t init_size = (off_t)stack_arg(f->esp, 2);
   f->eax = filesys_create(name, init_size);
+
+  lock_release(&filesys_lock);
 }
 
+static void open_handler(struct intr_frame *f) {
+  lock_acquire(&filesys_lock);
+ 
+  char *filename = (char*)stack_arg(f->esp, 1);           /* filename. */
+
+  struct file *file = filesys_open (filename);            /* open the file. */
+  if (file != NULL) {
+    struct fd_elem *elem = malloc (sizeof (*elem));       /* reserve mem for file descriptor. */
+    if (elem != NULL) {
+      elem->file = file;
+      elem->fd = next_fd++;
+      list_push_front(&fds, &elem->elem);                 /* register the file descriptor. */
+      f->eax = elem->fd;                                  /* return the file descriptor. */
+      goto end;
+    } else {
+      file_close(file);
+    }
+  }
+  
+  f->eax = -1;                                            /* something went wrong. */
+
+end:
+  lock_release(&filesys_lock);
+}
